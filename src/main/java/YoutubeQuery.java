8b60dc6884169adeda4e18f12037fd95bb54153d
Saman Tavassoli Jahromi <samandevelopper1@gmail.com>
2020-05-27 14:24:24 +0200

    Revert "Merge branch 'branch-saman' of https://github.com/SamanTavassoli/Youtube-Video-Recommender into branch-saman"
    This reverts commit 8fa5805b1308ce3405e8b9be30a0b95435ed2e29, reversing
    changes made to dad1c76cb6dfc649752a56a2ae800619c02da55d.

--- src/main/java/YoutubeQuery.java
+++ src/main/java/YoutubeQuery.java
@@ -8,24 +8,15 @@ import com.google.api.services.youtube.model.SearchResult;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-<<<<<<< HEAD
 import java.util.List;
 import java.util.Scanner;
-=======
-import java.util.ArrayList;
-import java.util.List;
->>>>>>> fa737c8ba26415d260de210da642776387cee7d5
 
 
 public class YoutubeQuery {
 
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String APPLICATION_NAME = "YOUTUBE-VIDEO-RECOMMENDER";
-<<<<<<< HEAD
     private static String API_KEY;
-=======
-    private static final String API_KEY = "***REMOVED***";
->>>>>>> fa737c8ba26415d260de210da642776387cee7d5
 
 
     public static void main(String[] args) throws GeneralSecurityException, IOException {
@@ -50,12 +41,9 @@ public class YoutubeQuery {
 
         // setting the request type and setting api key for credentials
         YouTube.Search.List request = youTubeService.search().list("snippet");
-<<<<<<< HEAD
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter API KEY: ");
         API_KEY = scanner.nextLine();
-=======
->>>>>>> fa737c8ba26415d260de210da642776387cee7d5
         request.setKey(API_KEY);
 
         // fetching response for "food"
