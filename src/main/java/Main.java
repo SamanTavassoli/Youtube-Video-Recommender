dad1c76cb6dfc649752a56a2ae800619c02da55d
Saman Tavassoli Jahromi <samandevelopper1@gmail.com>
2020-05-27 13:48:28 +0200

    Fix API KEY

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
