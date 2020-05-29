b60ffe4c96d5f6c408f10091514f98678c66615d
Saman Tavassoli Jahromi <samandevelopper1@gmail.com>
2020-05-29 11:44:21 +0200

    Add search for tags and parsing with json

--- src/main/java/YoutubeQuery.java
+++ src/main/java/YoutubeQuery.java
@@ -5,6 +5,7 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -27,10 +28,36 @@ public class YoutubeQuery {
         }
     }
     public static String[] getRecommendationsForTags(String[] tags) {
-        return new String[0];
+        try {
+            return parseResults(getResults(createQuerySearchTerm(tags)));
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+            System.err.println("No results were obtained from search");
+            return new String[0];
+        }
+
     }
 
-    private static String[] getResults(String[] args) throws GeneralSecurityException, IOException {
+    /**
+     * ORs all the tags together so the search can contain either tags
+     */
+    private static String createQuerySearchTerm(String[] tags) {
+        return String.join("|", tags);
+    }
+
+    private static String[] parseResults(String[] results) {
+        for (int i = 0; i < results.length; i++) {
+            String jsonString = results[i];
+            JSONObject obj = new JSONObject(jsonString);
+            String videoTitlte = obj.getJSONObject("snippet").getString("title");
+            String videoId = "https://www.youtube.com/watch?v=" + obj.getJSONObject("id").getString("videoId");
+            String result = "[\"" + videoTitlte + "\", " + videoId + "]";
+            results[i] = result;
+        }
+        return results;
+    }
+
+    private static String[] getResults(String querySearchTerm) throws GeneralSecurityException, IOException {
 
         final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
@@ -43,12 +70,12 @@ public class YoutubeQuery {
         YouTube.Search.List request = youTubeService.search().list("snippet");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter API KEY: ");
-        API_KEY = scanner.nextLine();
+        API_KEY = scanner.nextLine().trim();
         request.setKey(API_KEY);
 
-        // fetching response for "food"
-        SearchListResponse response = request.setMaxResults(25L)
-                .setQ("food")
+        // fetching response for fed in argument
+        SearchListResponse response = request.setMaxResults(5L)
+                .setQ(querySearchTerm)
                 .execute();
 
         // Putting all the responses into an array
